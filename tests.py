2c095e06168843c0af6ffe0ab515af0cb23ba6f8
Andrey Kulinych <vip.kulinych@mail.ru>
2024-06-09 16:45:32 +1100

    My first commit

--- tests.py
+++ tests.py
@@ -1,24 +1,71 @@
+import pytest
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
+
 class TestBooksCollector:
-
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
+    @pytest.mark.parametrize('name, expected_result', [
+        ("", False),
+        ("B" * 41, False),
+        ("B" * 40, True),
+        ("B", True),
+    ])
+    def test_add_new_book_add_books_with_different_limit_values(self, name, expected_result):
         collector = BooksCollector()
+        collector.add_new_book(name)
+        assert len(collector.books_genre) == expected_result
 
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+    def test_set_book_genre_successfully_added(self):
+        collector = BooksCollector()
+        collector.add_new_book('Book_1')
+        collector.set_book_genre('Book_1', 'Фантастика')
+        assert collector.books_genre['Book_1'] == 'Фантастика'
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+    def test_get_book_genre_use_name_in_books_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book('Book_1')
+        collector.set_book_genre('Book_1', 'Фантастика')
+        assert collector.get_book_genre('Book_1') == 'Фантастика'
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_get_books_with_specific_genre_use_setting_genre_and_search_books_by_this_value(self):
+        collector = BooksCollector()
+        collector.add_new_book('Book_1')
+        collector.add_new_book('Book_2')
+        collector.set_book_genre('Book_1', 'Фантастика')
+        collector.set_book_genre('Book_2', 'Фантастика')
+        assert collector.get_books_with_specific_genre('Фантастика') == ['Book_1', 'Book_2']
+
+    def test_get_books_genre_add_one_book(self):
+        collector = BooksCollector()
+        collector.add_new_book('Book_1')
+        collector.set_book_genre('Book_1', 'Фантастика')
+        assert collector.books_genre == {'Book_1': 'Фантастика'}
+
+    def test_get_books_for_children_add_book_with_raiting_and_without_raiting_added_without_raiting(self):
+        collector = BooksCollector()
+        collector.add_new_book('Book_1')
+        collector.add_new_book('Book_2')
+        collector.set_book_genre('Book_1', 'Фантастика')
+        collector.set_book_genre('Book_2', 'Ужасы')
+        assert collector.get_books_for_children() == ['Book_1']
+
+    def test_add_book_in_favorites_add_one_book(self):
+        collector = BooksCollector()
+        collector.add_new_book('Book_1')
+        collector.add_book_in_favorites('Book_1')
+        assert 'Book_1' in collector.get_list_of_favorites_books()
+
+    def test_delete_book_from_favorites_remove_one_book(self):
+        collector = BooksCollector()
+        collector.add_new_book('Book_1')
+        collector.add_book_in_favorites('Book_1')
+        collector.delete_book_from_favorites('Book_1')
+        assert 'Book_1' not in collector.get_list_of_favorites_books()
+
+    def test_get_list_of_favorites_books_add_two_book_in_favorite_getting_two_book(self):
+        collector = BooksCollector()
+        collector.add_new_book('Book_1')
+        collector.add_new_book('Book_2')
+        collector.add_book_in_favorites('Book_1')
+        collector.add_book_in_favorites('Book_2')
+        assert collector.get_list_of_favorites_books() == ['Book_1', 'Book_2']
+
